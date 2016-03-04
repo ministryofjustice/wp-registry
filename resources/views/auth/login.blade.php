954466e87a347d5e393743d0a2c03712c546c578
Ollie Treend <ollie@treend.com>
2016-03-04 13:59:23 +0000

    Update login page to use a title in the panel heading.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,9 @@
 <div class="container">
     <div class="login-box">
         <div class="panel panel-default">
-            <div class="panel-heading">Login</div>
+            <div class="panel-heading">
+                <h3 class="panel-title">Login</h3>
+            </div>
             <div class="panel-body{{ $errors->has('socialite') ? ' has-error' : '' }}">
                 <p>Please sign in with your MOJ Digital account.</p>
                 <a href="{{ route('login.google') }}" class="btn btn-lg btn-block btn-social btn-google">
